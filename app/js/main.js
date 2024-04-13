5cefbdff573b25bc20e37cdb8c842fdb9b4b6587
igmrsky <mrinskiy0282@mail.ru>
2024-04-13 22:23:37 +0400

    added tabs, next accardeon

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(function () {
     
+    $('.program__tab-link').on('click', function(e){
+        e.preventDefault();
+        const tabId = $(this).attr('href');
+
+        $('.program__tab-link').removeClass('program__tab-link--active');
+        $(this).addClass('program__tab-link--active');
+
+        if ($('.program__tab-item').hasClass('program__tab-item--active')) {
+            $('.program__tab-item').removeClass('program__tab-item--active');
+        }
+        $(tabId).addClass('program__tab-item--active')
+
+    });
+
 });
